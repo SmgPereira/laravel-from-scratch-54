c0b5b9cd89e9d49b8a16e29fde310fefa0250e74
Sergio Pereira <sergio@sergio.pereira>
2017-08-18 12:39:53 +0100

    ex16

--- app/Post.php
+++ app/Post.php
@@ -8,9 +8,18 @@ class Post extends Model
 {
     protected $guarded = [];                //or    protected $fillable = ['title', 'body'];
 
-
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function addComment($body)
+    {
+
+        $this->comments()->create(compact('body'));
+/*        Comment::create([
+            'body' => $body,
+            'post_id' => $this->id
+        ]);*/
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,7 +18,6 @@
 
                         <strong>
                             {{ $comment->created_at->diffForHumans() }}: &nbsp;
-
                         </strong>
 
                         {{ $comment->body }}
@@ -28,7 +27,27 @@
             </ul>
         </div>
 
+        {{-- Add a comment --}}
+        <hr>
+
+        <div class="card">
+            <div class="card-block">
+                <form method="POST" action="/posts/{{ $post->id }}/comments">
+                    {{ csrf_field() }}
+
+                    <div class="form-group">
+                        <textarea name="body" placeholder="Your comment here." class="form-control" required></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary">Add Comment</button>
+                    </div>
+                </form>
+
+                @include('layouts.errors')
+
+            </div>
+        </div>
+
     </div>
-
-
 @endsection
