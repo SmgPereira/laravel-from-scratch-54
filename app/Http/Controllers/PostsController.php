caddd92fd4f568e304d15e0b437f619911b2ada9
Sergio Pereira <sergio@sergio.pereira>
2017-08-21 17:15:12 +0100

    ex21

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -17,21 +17,7 @@ class PostsController extends Controller
            ->filter(request(['month', 'year']))
            ->get();
 
-//       $posts = Post::latest();
-//          if ($month = request('month')) {
-//              $posts->whereMonth('created_at', Carbon::parse($month)->month);
-//       }
-//          if ($year = request('year')) {
-//              $posts->whereYear('created_at', $year);
-//       }
-//       $posts = $posts->get();
-
-        // Temporary.
-       $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-           ->groupBy('year', 'month')
-           ->orderByRaw('min(created_at) desc')
-           ->get()
-           ->toArray();
+       $archives = Post::archives();
 
        return view('posts.index', compact('posts', 'archives'));
    }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //posto por mim devido a (laravel-news.com/laravel-5-4-key-too-long-error)
         Schema::defaultStringLength(191);
+
+        view()->composer('layouts.sidebar', function ($view) {
+            $view->with('archives', \App\Post::archives());
+        });
+
     }
 
     /**
