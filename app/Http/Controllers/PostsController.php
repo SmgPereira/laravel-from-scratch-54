195772ae87051fe20571ecda2524fd57727c2356
Sergio Pereira <sergio@sergio.pereira>
2017-08-21 15:54:25 +0100

    ex20

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -18,13 +18,13 @@ class PostsController extends Controller
            ->get();
 
 //       $posts = Post::latest();
-////       if ($month = request('month')) {
-//           $posts->whereMonth('created_at', Carbon::parse($month)->month);
+//          if ($month = request('month')) {
+//              $posts->whereMonth('created_at', Carbon::parse($month)->month);
 //       }
-//       if ($year = request('year')) {
-//           $posts->whereYear('created_at', $year);
+//          if ($year = request('year')) {
+//              $posts->whereYear('created_at', $year);
 //       }
-////       $posts = $posts->get();
+//       $posts = $posts->get();
 
         // Temporary.
        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
