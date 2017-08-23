35ec30454000eaec62fc187c6f690a2d01431829
Sergio Pereira <sergio@sergio.pereira>
2017-08-23 16:19:31 +0100

    ex31

--- app/Tag.php
+++ app/Tag.php
@@ -8,8 +8,13 @@ class Tag extends Model
 {
     public function posts()
     {
-        $this->belongsToMany(Post::class);
+        return $this->belongsToMany(Post::class);
     }
 
+    public function getRouteKeyName()
+    {
+        return 'name';
+
+    }
 
 }
